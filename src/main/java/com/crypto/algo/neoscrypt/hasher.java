21b6708af13c9ca035509a29fc17eea90f7a2d8b
Wellenreiter01 <wellenreiter01@t-online.de>
2015-01-30 15:10:26 +0100

    modified test class to use newer junit module

--- src/main/java/com/crypto/algo/neoscrypt/hasher.java
+++ src/main/java/com/crypto/algo/neoscrypt/hasher.java
@@ -1,37 +1,55 @@
 
 package com.crypto.algo.neoscrypt;
 
-/*
+
+/* TODO do we need log information?
+* logger not available at runtime value add is limted
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 */
 public class hasher {
-
+    /**
+     * The wrapper around the original neoscrypt c-code
+     * loads the native library passes program parameter to the c-code
+     * and returns the result
+     */
     private static boolean native_library_loaded = false;
- //  private static Logger log = LoggerFactory.getLogger(hasher.class);
+  //  private static Logger log = LoggerFactory.getLogger(hasher.class);
 
         static {
             try {
                 System.loadLibrary("neoscrypt");
                 native_library_loaded = true;
             } catch (Throwable e) {
-   //            log.error("native library neoscrypt not loaded" ,e);
+          //    log.error("native library neoscrypt not loaded" ,e);
             }
         }
 
 	public static char[] getHash(char[] input, int profile) {
+        /**
+         * calls the c- code
+         * @parameters:
+         * input: string to be hashed
+         * profile: defines which hash algorithm to use
+         *  - 0x3 : Scrypt
+         *  - 0x80000620 : neoscrypt for feathercoin
+         */
 		hasher neoScrypt = new hasher();
+        char[] output = new char[32];
+
 
         if (native_library_loaded) {
+/* Todo: is char the right data type to pass?
 
-            char[] output = input.clone();
+ */
             neoScrypt.neoscrypt(input, output, profile);
           //  log.warn ( "output: ", output);
+          //  return neoScrypt.neoscrypt(input, output, profile);
             return output;
         }
         return input;
 	}
 
-	private native void neoscrypt(char[] input,char[] output,int profile);
+	private native char[] neoscrypt(char[] input,char[] output,int profile);
 
 }
--- src/test/java/com/crypto/algo/hasher_Test.java
+++ src/test/java/com/crypto/algo/hasher_Test.java
@@ -9,19 +9,20 @@ package com.crypto.algo;
 
 import com.crypto.algo.neoscrypt.hasher;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 
 import org.junit.Test;
-
 
 
 public class hasher_Test {
 /* reference taken from qt-wallet code
 */
-    private static char[] reference = new char[]{0x72, 0x58, 0x96, 0x1A, 0xFB, 0x33, 0xFD, 0x12,
+   private static char[] reference = new char[]{0x72, 0x58, 0x96, 0x1A, 0xFB, 0x33, 0xFD, 0x12,
             0xD0, 0x0C, 0xAC, 0xB8, 0xD6, 0x3F, 0x4F, 0x4F,
             0x52, 0xBB, 0x69, 0x17, 0x04, 0x38, 0x65, 0xDD,
             0x24, 0xA0, 0x8F, 0x57, 0x88, 0x53, 0x12, 0x2D};
+
+
 
 
     private static char[] input= {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
@@ -34,19 +35,19 @@ public class hasher_Test {
             71, 72, 73, 74, 75, 76, 77, 78, 79, 70
     };
 
-
-    int i;
-
+    private char[] reborn = new char[32];
 
 
 
     @Test
    public  void neoscryptTest() throws Exception {
 
-       // for (i=0; i<80;input[i] = {i});
-       char[] reborn = hasher.getHash(input, 0x80000620);
-        org.junit.Assert.assertEquals("LengthTest",reference.length,reborn.length);
-        Assert.assertEquals("HashComparison",reference.toString(), (reborn.toString()));
+       // for (i=0; i<80;input[i] = {i})
+        Assert.assertEquals("LengthTest1", reference.length, reborn.length);
+       reborn = hasher.getHash(input, 0x80000620);
+       Assert.assertEquals("LengthTest2", reference.length, reborn.length);
+       Assert.assertEquals("NeoscryptResult:", reference.hashCode(), input.hashCode());
+
    }
 
 }
